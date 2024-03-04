ce15930fcfe6d0ec858b3a18f0925ae660076955
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-04 15:40:33 +0000

    updated template

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
                         <img src="images/component-placeholder-square.jpg">
                         <div class="collection-overlay">
                             <h4>Estate Gates</h4>
-                            <a href="#">View Collection</a>
+                            <a href="#">View Our Collection</a>
                         </div>
                         <h4>Estate Gates</h4>
                     </li>
@@ -119,7 +119,7 @@
                         <img src="images/component-placeholder-square.jpg">
                         <div class="collection-overlay">
                             <h4>Side &amp; Garden Gates</h4>
-                            <a href="#">View Collection</a>
+                            <a href="#">View Our Collection</a>
                         </div>
                         <h4>Side &amp; Garden Gates</h4>
                     </li>
@@ -127,7 +127,7 @@
                         <img src="images/component-placeholder-square.jpg">
                         <div class="collection-overlay">
                             <h4>Balustrades &amp; Staircases</h4>
-                            <a href="#">View Collection</a>
+                            <a href="#">View Our Collection</a>
                         </div>
                         <h4>Balustrades &amp; Staircases</h4>
                     </li>
@@ -135,7 +135,7 @@
                         <img src="images/component-placeholder-square.jpg">
                         <div class="collection-overlay">
                             <h4>Aluminium Gates</h4>
-                            <a href="#">View Collection</a>
+                            <a href="#">View Our Collection</a>
                         </div>
                         <h4>Aluminium Gates</h4>
                     </li>
@@ -143,6 +143,226 @@
 
             </div>
         </div><!-- end collections -->
+
+        <div class="textimage-col light-theme">
+            <div class="c-container">
+
+                <div class="image-col">
+                    <img src="images/component-placeholder-square.jpg">
+                </div>
+
+                <div class="text-col">
+
+                    <h4>Wrought Iron Railings</h4>
+                    <p>
+                        Duis erat odio, mollis at imperdiet quis, laoreet et elit. Nunc sagittis et tortor vehicula pharetra. 
+                        Cras aliquam eros eget enim auctor, tincidunt hendrerit velit sagittis. Mauris at mauris at magna consectetur semper. 
+                        Etiam semper pretium arcu non rutrum. Maecenas semper et arcu id posuere.
+                    </p>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum. Sed a pellentesque urna. Sed convallis non nisi non pharetra. 
+                        Maecenas facilisis sapien eget mollis laoreet.
+                    </p>
+
+                    <a href="#" class="orange-solid">Explore our wrought iron railings</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="collection-grid">
+            <div class="c-container">
+
+                <div class="col-large slider">
+                    <ul>
+                        <li>
+                            <picture>
+                                <source media="(max-width:767px)" srcset="images/component-placeholder-mob.jpg"><!-- Image for mobile devices -->        
+                                <img src="images/component-placeholder-square.jpg" alt=""><!-- Image for desktop screens -->
+                            </picture>
+                            <div class="slider-content">
+                                <div class="content-box">
+                                    <h3 class="slide-title">Privacy Gates</h3>
+                                    <p>Hardwood gates...the best of both worlds</p>
+                                    <a href="#" class="orange-solid">View Our Collection</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <picture>
+                                <source media="(max-width:767px)" srcset="images/component-placeholder-mob.jpg"><!-- Image for mobile devices -->        
+                                <img src="images/component-placeholder-square.jpg" alt=""><!-- Image for desktop screens -->
+                            </picture>
+                            <div class="slider-content">
+                                <div class="content-box">
+                                    <h3 class="slide-title">Slide Two</h3>
+                                    <p>Add your text here...</p>
+                                    <a href="#" class="orange-solid">Button Two</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <div class="slider-nav">
+                        <a class="prev-slide"><img src="images/dark-arrow2.png"></a>
+                        <a class="next-slide"><img src="images/dark-arrow.png"></a>
+                    </div>
+
+                </div>
+
+                <div class="col-slim">
+
+                    <ul>
+
+                        <li>
+                            <img src="images/component-placeholder.jpg">
+                            <div class="collection-overlay">
+                                <h4>Wood Gates</h4>
+                                <a href="#">View Our Collection</a>
+                            </div>
+                            <h4>Wood Gates</h4>
+                        </li>
+
+                        <li>
+                            <img src="images/component-placeholder.jpg">
+                            <div class="collection-overlay">
+                                <h4>Aluminium Gates</h4>
+                                <a href="#">View Our Collection</a>
+                            </div>
+                            <h4>Aluminium Gates</h4>
+                        </li>
+
+                        <li>
+                            <img src="images/component-placeholder.jpg">
+                            <div class="collection-overlay">
+                                <h4>Composite Gates</h4>
+                                <a href="#">View Our Collection</a>
+                            </div>
+                            <h4>Composite Gates</h4>
+                        </li>
+
+                </div>
+
+            </div>
+        </div><!-- end collection grid -->
+
+        <div class="textimage-col light-theme is-reversed">
+            <div class="c-container">
+
+                <div class="image-col">
+                    <img src="images/component-placeholder-square.jpg">
+                </div>
+
+                <div class="text-col">
+
+                    <p class="subheading">Our process</p>
+                    <h4>From drawing board to reality...<br>The only limit is your imagination</h4>
+                    <p>
+                        Duis erat odio, mollis at imperdiet quis, laoreet et elit. Nunc sagittis et tortor vehicula pharetra. 
+                        Cras aliquam eros eget enim auctor, tincidunt hendrerit velit sagittis. Mauris at mauris at magna consectetur semper. 
+                    </p>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum.
+                    </p>
+
+                    <a href="#" class="orange-solid">Discover our process</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="information-callout" style="background-image: url('images/component-placeholder.jpg">
+
+            <h4 class="callout-heading">Wrought Iron Gates and Railings</h4>
+            <p>For more information about our range of beautiful wrought iron gates.</p>
+            <a href="#" class="white-outline">Request A Brochure</a>
+            <a href="#" class="orange-solid">Make An Enquiry</a>
+
+        </div><!-- end information callout -->
+
+        <div class="installs-grid">
+
+            <h4>See our newest gate designs &amp; installs</h4>
+
+            <ul class="multiple-images">
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+            </ul>
+
+            <div class="single-image">
+                <img src="images/component-placeholder-square.jpg">
+            </div>
+
+            <div class="install-btn">
+                <a href="#" class="orange-solid">View All New Installs</a>
+            </div>
+
+        </div><!-- end installs grid -->
+
+        <div class="textimage-col light-theme">
+            <div class="c-container">
+
+                <div class="video-col">
+                    <a href="#"><img src="images/component-placeholder-square.jpg"></a>
+                </div>
+
+                <div class="text-col">
+
+                    <p class="subheading">Visit our Showroom</p>
+                    <h4>Need inspiration or want to the see the quality of our work for yourselves?</h4>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum.
+                    </p>
+
+                    <a href="#" class="orange-solid">Visit our showroom</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+            </div>
+
+        </div><!-- end instagram feed -->
 
     </main>
 
