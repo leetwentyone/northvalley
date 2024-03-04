9e65350dd61546f1f9c9b5c638fa6c675a476e9b
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-04 12:21:03 +0000

    updated template

--- index.php
+++ index.php
@@ -1,8 +1,148 @@
 <?php include('includes/header.php'); ?>
 
-    <main class="site-content">
+    <main class="site-content body-home">
 
-        Main
+        <div class="hero">
+
+            <picture>
+                <source media="(max-width:767px)" srcset="images/component-placeholder-mob.jpg"><!-- Image for mobile devices -->        
+                <img src="images/component-placeholder.jpg" alt=""><!-- Image for desktop screens -->
+            </picture>
+
+            <div class="hero-overlay">
+                <div class="overlay-content">
+                    <p>North Valley Forge</p>
+                    <h1>UK's premier Electric<br>Gate Specialist</h1>
+                    <a href="#">Explore Our Iron Gates</a>
+                </div>
+            </div><!-- end hero overlay -->
+
+        </div><!-- end hero -->
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h2>Quality wrought Iron Electric Gates &amp; Railings</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                    Curabitur vitae elit porta, imperdiet quam at, volutpat neque.
+                </p>
+                <div class="textblock-btns">
+                    <a href="#" class="orange-outline">Button One</a>
+                    <a href="#" class="orange-solid">Button Two</a>
+                </div>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="slider">
+        
+            <ul>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/component-placeholder-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/component-placeholder.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Ultimate Collection</h3>
+                            <p>Anything else is just a gate...</p>
+                            <a href="#" class="orange-solid">View Collection</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/component-placeholder-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/component-placeholder.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Slide Two</h3>
+                            <p>Add your text here...</p>
+                            <a href="#" class="orange-solid">Button Two</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="slider-nav">
+                <a class="prev-slide"><img src="images/dark-arrow2.png"></a>
+                <a class="next-slide"><img src="images/dark-arrow.png"></a>
+            </div>          
+
+        </div><!-- end slider -->
+
+        <div class="texblock-2col">
+            <div class="c-container">
+
+                <div class="col-1">
+                    <h4>Quality Iron Gates Design,<br>Wooden Driveway Gates,<br>Side Gates &amp; Railings.</h4>
+                    <a href="#" class="orange-solid">Request Brochure</a>
+                </div>
+
+                <div class="col-2">
+                    <p>
+                        Nam id sapien ac purus fringilla malesuada at vitae nunc. Praesent mattis fermentum felis, eu dictum nibh feugiat in. 
+                        Nulla sagittis vestibulum ullamcorper. Nunc ante velit, maximus sed semper at, 
+                        suscipit eget augue. Nam rutrum augue at velit venenatis porta.
+                    </p>
+                    <p>
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ullamcorper imperdiet risus, 
+                        nec consequat nisi venenatis id. Nullam ac blandit tellus. Sed hendrerit mollis nibh ac commodo. 
+                        In odio diam, varius ac erat vitae, pellentesque dignissim libero.
+                    </p>
+                    <p>
+                        Proin mollis ut est vitae dapibus. Morbi tincidunt varius elit, id tincidunt quam vestibulum dictum. 
+                        Donec congue nunc sed tincidunt convallis. Ut feugiat fringilla rutrum.
+                    </p>
+                </div>
+
+            </div>
+        </div><!-- end 2 column text block -->
+
+        <div class="collections">
+            <div class="c-container">
+
+                <ul>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Estate Gates</h4>
+                            <a href="#">View Collection</a>
+                        </div>
+                        <h4>Estate Gates</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Side &amp; Garden Gates</h4>
+                            <a href="#">View Collection</a>
+                        </div>
+                        <h4>Side &amp; Garden Gates</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Balustrades &amp; Staircases</h4>
+                            <a href="#">View Collection</a>
+                        </div>
+                        <h4>Balustrades &amp; Staircases</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Aluminium Gates</h4>
+                            <a href="#">View Collection</a>
+                        </div>
+                        <h4>Aluminium Gates</h4>
+                    </li>
+                </ul>
+
+            </div>
+        </div><!-- end collections -->
 
     </main>
 
