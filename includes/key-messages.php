26f9ccf9793ef5dcac3caf8bd6d0a10e88393f01
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-04 12:22:00 +0000

    key messages include template

---
+++ includes/key-messages.php
@@ -0,0 +1,20 @@
+<div class="key-messages">
+    <ul>
+        <li>
+            <img src="images/tick.png">
+            <p>Five Star Rated</p>
+        </li>
+        <li>
+            <img src="images/tick.png">
+            <p>Concept to Completion</p>
+        </li>
+        <li>
+            <img src="images/tick.png">
+            <p>Handmade Luxury Gates</p>
+        </li>
+        <li>
+            <img src="images/tick.png">
+            <p>Superior Quality</p>
+        </li>
+    </ul>
+</div>
