dc6fd4739a135dc14f7d5d58ca72722499f83809
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-05 14:36:11 +0000

    new template

---
+++ casestudies.php
@@ -0,0 +1,164 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-casestudies" id="top">
+
+        <div class="slider">
+            
+            <ul>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Case Study One</h3>
+                            <p>Morbi congue scelerisque diam, ac tempor libero dapibus nec.</p>
+                            <a href="#" class="orange-solid">View Case Study</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Case Study Two</h3>
+                            <p>Morbi congue scelerisque diam, ac tempor libero dapibus nec.</p>
+                            <a href="#" class="orange-solid">View Case Study</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="slider-nav">
+                <a class="prev-slide"><img src="images/dark-arrow2.png"></a>
+                <a class="next-slide"><img src="images/dark-arrow.png"></a>
+            </div>          
+
+        </div><!-- end slider -->
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h2>Case Studies</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                    Curabitur vitae elit porta, imperdiet quam at, volutpat neque.
+                </p>
+            
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="collections">
+            <div class="c-container">
+
+                <ul>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Case Study Name</h4>
+                            <a href="#">View Case Study</a>
+                        </div>
+                        <h4>Case Study Name</h4>
+                    </li>
+                </ul>
+
+                <div class="install-btn">
+                    <a href="#" class="orange-solid">Load more case studies</a>
+                </div>
+
+            </div>
+        </div><!-- end collections -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+                <a href="#" class="orange-solid">Follow Us</a>
+
+            </div>
+
+        </div><!-- end instagram feed -->
+
+    </main>
+
+<?php include('includes/footer.php'); ?>
