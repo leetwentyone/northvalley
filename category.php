f1f89c3d3820510f100fa3aecea963821f61d674
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-05 10:38:37 +0000

    new template

---
+++ category.php
@@ -0,0 +1,212 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-category" id="top">
+
+        <div class="slider">
+            
+            <ul>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Ultimate Collection</h3>
+                            <p>Anything else is just a gate...</p>
+                            <a href="#" class="orange-solid">View Collection</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Slide Two</h3>
+                            <p>Add your text here...</p>
+                            <a href="#" class="orange-solid">Button Two</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="slider-nav">
+                <a class="prev-slide"><img src="images/dark-arrow2.png"></a>
+                <a class="next-slide"><img src="images/dark-arrow.png"></a>
+            </div>          
+
+        </div><!-- end slider -->
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h2>Wrought Iron Gates</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                    Curabitur vitae elit porta, imperdiet quam at, volutpat neque.
+                </p>
+                <div class="textblock-btns">
+                    <a href="#" class="orange-outline">Request Brochure</a>
+                    <a href="#" class="orange-solid">Make An Enquiry</a>
+                </div>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="collections">
+            <div class="c-container">
+
+                <ul>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Estate Gates</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Estate Gates</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Side &amp; Garden Gates</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Side &amp; Garden Gates</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Balustrades &amp; Staircases</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Balustrades &amp; Staircases</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Aluminium Gates</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Aluminium Gates</h4>
+                    </li>
+                </ul>
+
+            </div>
+        </div><!-- end collections -->
+
+        <div class="textimage-col light-theme">
+            <div class="c-container">
+
+                <div class="image-col">
+                    <img src="images/placeholder-img1.jpg">
+                </div>
+
+                <div class="text-col">
+
+                    <h4>SEO Content Block</h4>
+                    <p>
+                        Duis erat odio, mollis at imperdiet quis, laoreet et elit. Nunc sagittis et tortor vehicula pharetra. 
+                        Cras aliquam eros eget enim auctor, tincidunt hendrerit velit sagittis. Mauris at mauris at magna consectetur semper. 
+                        Etiam semper pretium arcu non rutrum. Maecenas semper et arcu id posuere.
+                    </p>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum. Sed a pellentesque urna. Sed convallis non nisi non pharetra. 
+                        Maecenas facilisis sapien eget mollis laoreet.
+                    </p>
+
+                    <a href="#" class="orange-solid">Explore our Iron Gates</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="installs-grid">
+
+            <h4>See our newest gate designs &amp; installs</h4>
+
+            <ul class="multiple-images">
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+                <li>
+                    <a href="#"></a>
+                    <img src="images/component-placeholder-square.jpg">
+                </li>
+            </ul>
+
+            <div class="single-image">
+                <img src="images/component-placeholder-square.jpg">
+            </div>
+
+            <div class="install-btn">
+                <a href="#" class="orange-solid">View All New Installs</a>
+            </div>
+
+        </div><!-- end installs grid -->
+
+        <div class="textimage-col light-theme">
+            <div class="c-container">
+
+                <div class="video-col">
+                    <a href="#"><img src="images/placeholder-img1.jpg"></a>
+                </div>
+
+                <div class="text-col">
+
+                    <p class="subheading">Visit our Showroom</p>
+                    <h4>Need inspiration or want to the see the quality of our work for yourselves?</h4>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum.
+                    </p>
+
+                    <a href="#" class="orange-solid">Visit our showroom</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+                <a href="#" class="orange-solid">Follow Us</a>
+
+            </div>
+
+        </div><!-- end instagram feed -->
+
+    </main>
+
+<?php include('includes/footer.php'); ?>
