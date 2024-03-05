3dc8f3ca5462d4c79daf4ae947dcd0b1bd248f3d
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-05 10:52:16 +0000

    new template

---
+++ sub-category.php
@@ -0,0 +1,193 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-subcategory" id="top">
+
+        <div class="slider">
+            
+            <ul>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Cranleigh</h3>
+                            <p>Our traditional driveway and estate gates are designed to suit any style of property</p>
+                            <a href="#" class="orange-solid">View Our Collection</a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <picture>
+                        <source media="(max-width:767px)" srcset="images/placeholder-img2-mob.jpg"><!-- Image for mobile devices -->        
+                        <img src="images/placeholder-img2.jpg" alt=""><!-- Image for desktop screens -->
+                    </picture>
+                    <div class="slider-content">
+                        <div class="content-box">
+                            <h3 class="slide-title">Slide Two</h3>
+                            <p>Add your text here...</p>
+                            <a href="#" class="orange-solid">Button Two</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="slider-nav">
+                <a class="prev-slide"><img src="images/dark-arrow2.png"></a>
+                <a class="next-slide"><img src="images/dark-arrow.png"></a>
+            </div>          
+
+        </div><!-- end slider -->
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h2>Driveway Estate Gates</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                    Curabitur vitae elit porta, imperdiet quam at, volutpat neque.
+                </p>
+                <div class="textblock-btns">
+                    <a href="#" class="orange-outline">Request Brochure</a>
+                    <a href="#" class="orange-solid">Make An Enquiry</a>
+                </div>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="collections">
+            <div class="c-container">
+
+                <ul>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Cranleigh</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Cranleigh</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Hambledon</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Hambledon</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Blenheim</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Blenheim</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Warwick</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Warwick</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Radleigh</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Radleigh</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Langley</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Langley</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Somerton</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Somerton</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Salisbury</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Salisbury</h4>
+                    </li>
+                    <li>
+                        <img src="images/component-placeholder-square.jpg">
+                        <div class="collection-overlay">
+                            <h4>Huntington</h4>
+                            <a href="#">View Our Collection</a>
+                        </div>
+                        <h4>Huntington</h4>
+                    </li>
+                </ul>
+
+                <div class="install-btn">
+                    <a href="#" class="orange-solid">View All</a>
+                </div>
+
+            </div>
+        </div><!-- end collections -->
+
+        <div class="textimage-col light-theme">
+            <div class="c-container">
+
+                <div class="video-col">
+                    <a href="#"><img src="images/placeholder-img1.jpg"></a>
+                </div>
+
+                <div class="text-col">
+
+                    <p class="subheading">Visit our Showroom</p>
+                    <h4>Need inspiration or want to the see the quality of our work for yourselves?</h4>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum.
+                    </p>
+
+                    <a href="#" class="orange-solid">Visit our showroom</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+                <a href="#" class="orange-solid">Follow Us</a>
+
+            </div>
+
+        </div><!-- end instagram feed -->
+
+    </main>
+
+<?php include('includes/footer.php'); ?>
