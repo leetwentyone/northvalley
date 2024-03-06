6953cfe06877f444c2669a3b1df64634dcaecdd0
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-06 12:56:21 +0000

    updated template

--- error.php
+++ error.php
@@ -1,16 +1,16 @@
 <?php include('includes/header.php'); ?>
 
-    <main class="site-content body-thankyou" id="top">
+    <main class="site-content body-error" id="top">
 
         <?php include('includes/key-messages.php'); ?>
 
         <div class="textblock">
             <div class="c-container">
 
-                <h1>Thankyou</h1>
+                <h1>404</h1>
                 <p>
-                    Thankyou for your request. One of our team members will be in contact with you shortly, this could take up to 48hrs but if you need urgent assistance please
-                    contact us on <a href="tel:01282677300">01282 677 300</a>
+                    Sorry, the page you are looking for doesn't exist. Please try searching again or check your spelling. If you need urgent help please
+                    call us on <a href="tel:01282677300">01282 677300</a> or email us on <a href="mailto:sales@nvf.co.uk">sales@nvf.co.uk</a>
                 </p>
                 <div class="textblock-btns">
                     <a href="index.php" class="orange-solid">Back Home</a>
