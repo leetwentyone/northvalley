64a8c60f12908f3cc12b96829e1ddb5c1b57a477
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-06 10:26:08 +0000

    new template

--- contact.php
+++ contact.php
@@ -1,19 +1,134 @@
 <?php include('includes/header.php'); ?>
 
-    <main class="site-content body-showroom" id="top">
+    <main class="site-content body-contact" id="top">
 
         <?php include('includes/key-messages.php'); ?>
 
         <div class="textblock">
             <div class="c-container">
 
-                <h2>Europes largest <br> electric gate showroom</h2>
+                <h1>Contact Us</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                    In sagittis diam in lectus imperdiet venenatis. <a href="#">Aliquam quis magna aliquam</a>, porta nulla ac, consectetur ipsum. 
+                    Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
                 </p>
+                <p>We will add a phone number in this textblock <a href="tel:0800000000">0800 000 000</a></p>
 
             </div>
         </div><!-- end textblock -->
+
+        <div class="getintouch">
+            <div class="c-container">
+
+                <div class="col-l">
+
+                    <h2>Have questions? get in touch!</h2>
+
+                    <form>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Name*</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>Email Address*</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Telephone*</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>Postcode*</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+                                <label>Your Message</label>
+                                <textarea rows="5"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+                                <label>How did you hear about us?</label>
+                                <select>
+                                    <option>Search Engine</option>
+                                    <option>Word of mouth</option>
+                                    <option>Online Ad</option>
+                                    <option>Publication</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+                                <input type="checkbox">I agree to the privacy policy
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+                                <button type="submit">Submit</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="col-r">
+
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9415.04327583636!2d-2.2239723490085943!3d53.8471178723239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b912497657db1%3A0x6729f21e7ea7077e!2sNorth%20Valley%20Composites!5e0!3m2!1sen!2suk!4v1709719848684!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+
+                    <div class="contact-details">
+
+                        <h3>Contact Details</h3>
+
+                        <address>
+                            North Valley Forge Ltd,<br>
+                            Valley Forge Business Park,<br>
+                            Reedyford Road,<br>
+                            Nelson BB9 8TU
+                        </address>
+
+                        <a href="tel:01282677300">01282 677300</a><br>
+                        <a href="mailto:sales@nvf.co.uk">sales@nvf.co.uk</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div><!-- end get in touch -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+                <a href="#" class="orange-solid">Follow Us</a>
+
+            </div>
+
+        </div><!-- end instagram feed -->
 
     </main>
 
