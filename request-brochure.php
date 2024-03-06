6061bb1a317151fba47f985dd0613cf082d20dc3
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-06 10:45:04 +0000

    new template

---
+++ request-brochure.php
@@ -0,0 +1,124 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-request" id="top">
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h1>Request the north valley forge <br> wrought iron catalogue</h1>
+                <p>
+                    In sagittis diam in lectus imperdiet venenatis. <a href="#">Aliquam quis magna aliquam</a>, porta nulla ac, consectetur ipsum. 
+                    Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                </p>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="request">
+            <div class="c-container">
+
+                <div class="col-l">
+
+                    <img src="images/placeholder-img1.jpg">
+
+                </div>
+
+                <div class="col-r">
+
+                <h2>Catalogue by post</h2>
+                    <p>Fill in the details below to get a catalogue sent out in the post. Please allow up to 5 working days for it to arrive.</p>
+
+                    <form>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Name*</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>Email Address*</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Telephone*</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>Postcode*</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Address Line 1</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>Address Line 1</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row50">
+                                <label>Town</label>
+                                <input type="text">
+                            </div>
+                            <div class="row50">
+                                <label>County</label>
+                                <input type="email">
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="html-block">
+                                I would like to receive relevant email updates from NVF
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+
+                                <div class="checkbox-inline">
+                                    <div>
+                                        <input type="checkbox">Workshop Wednesday
+                                    </div>
+                                    <div>
+                                        <input type="checkbox">General updates
+                                    </div>
+                                    <div>
+                                        <input type="checkbox">Recent installs
+                                    </div>
+                                    <div>
+                                        <input type="checkbox">All emails
+                                    </div>
+                                    <div>
+                                        <input type="checkbox">Information &amp; Advice
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="row100">
+                                <button type="submit">Submit</button>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+        </div><!-- end get in touch -->
+        
+    </main>
+
+<?php include('includes/footer.php'); ?>
