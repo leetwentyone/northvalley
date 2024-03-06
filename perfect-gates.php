bebe6c1c5b6442fb4f2d97e9f153c385893bcde6
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-06 08:40:30 +0000

    new template

---
+++ perfect-gates.php
@@ -0,0 +1,265 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-perfectgates" id="top">
+
+        <div class="page-banner">
+
+            <picture>
+                <source media="(max-width:767px)" srcset="images/placeholder-img1-mob.jpg"><!-- Image for mobile devices -->        
+                <img src="images/placeholder-img1.jpg" alt=""><!-- Image for desktop screens -->
+            </picture>
+
+        </div><!-- end page banner --> 
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h2>Choosing the perfect gate</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada sapien et ultricies dictum. Nam at pretium mi. 
+                </p>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="textimage-col default-theme is-reversed">
+            <div class="c-container">
+
+                <div class="image-col">
+                    <img src="images/placeholder-img1.jpg">
+                </div>
+
+                <div class="text-col">
+
+                    <h4>What gate opening style will be best?</h4>
+                    <p>
+                        Duis erat odio, mollis at imperdiet quis, laoreet et elit. Nunc sagittis et tortor vehicula pharetra. 
+                        Cras aliquam eros eget enim auctor, tincidunt hendrerit velit sagittis. Mauris at mauris at magna consectetur semper. 
+                        Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. Curabitur condimentum arcu vitae blandit eleifend. 
+                        Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. Suspendisse eget libero porta, 
+                        porttitor mauris non, molestie odio.
+                    </p>
+
+                    <p>
+                        Mauris venenatis, dui sit amet suscipit porta, felis leo mollis massa, vel interdum arcu lacus sit amet eros. 
+                        Aliquam erat volutpat. Sed ultricies augue at erat volutpat sollicitudin. Aenean sit amet nunc diam. 
+                        Donec non risus vel felis molestie interdum.
+                    </p>
+
+                    <a href="#" class="orange-solid">Discover our process</a>
+
+                </div>
+
+            </div>
+        </div><!-- end text and image col -->
+
+        <div class="choose-gates">
+            <div class="c-container">
+
+                <ul class="multiple-images">
+                    <li>
+                        <a href="#"></a>
+                        <img src="images/component-placeholder-square.jpg">
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                        <img src="images/component-placeholder-square.jpg">
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                        <img src="images/component-placeholder-square.jpg">
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                        <img src="images/component-placeholder-square.jpg">
+                    </li>
+                </ul>
+
+                <div class="text-col">
+                    <h4>How to choose a gate design</h4>
+                    <p>
+                        In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                        Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                        Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                    </p>
+
+                    <p>
+                        Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                        Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                        Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                        Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                        ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                    </p>
+
+                    <p>
+                        Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                        Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                        Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                        Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                        Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                    </p>
+                </div>
+
+            </div>      
+        </div><!-- end choose gates -->
+
+        <div class="faqs">
+            <div class="c-container">
+
+                <div class="faqs-container">
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div><!-- end faqs -->
+
+        <div class="information-callout" style="background-image: url('images/placeholder-img2.jpg">
+
+            <h4 class="callout-heading">Wrought Iron Gates and Railings</h4>
+            <p>For more information about our range of beautiful wrought iron gates.</p>
+            <a href="#" class="white-outline">Request A Brochure</a>
+            <a href="#" class="orange-solid">Make An Enquiry</a>
+
+        </div><!-- end information callout -->
+
+        <div class="instagram">
+
+            <h4>The only limit is your imagination</h4>
+
+            <div class="c-container">
+
+                <ul>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                    <li><img src="images/component-placeholder-square.jpg"></li>
+                </ul>
+
+                <a href="#" class="orange-solid">Follow Us</a>
+
+            </div>
+
+        </div><!-- end instagram feed -->
+
+    </main>
+
+<?php include('includes/footer.php'); ?>
