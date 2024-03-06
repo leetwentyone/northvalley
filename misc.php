daf758e13d58c17ffed9d278de71454c1a6b5273
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-06 11:39:07 +0000

    new template

---
+++ misc.php
@@ -0,0 +1,150 @@
+<?php include('includes/header.php'); ?>
+
+    <main class="site-content body-misc" id="top">
+
+        <?php include('includes/key-messages.php'); ?>
+
+        <div class="textblock">
+            <div class="c-container">
+
+                <h1>Privacy</h1>
+
+            </div>
+        </div><!-- end textblock -->
+
+        <div class="faqs">
+            <div class="c-container">
+
+                <div class="faqs-container">
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac <a href="#">tempor libero dapibus nec</a>. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. <strong>Aliquam erat volutpat</strong>. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <ol>
+                                <li>This is a list item</li>
+                                <li>This is a list item</li>
+                                <li>This is a list item</li>
+                                <li>This is a list item</li>
+                            </ol>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="faq-item">
+
+                        <div class="faq-heading">This is the heading for this FAQ item</div>
+                        <div class="faq-answer" style="display:none;">
+                            <p>
+                                In sagittis diam in lectus imperdiet venenatis. Aliquam quis magna aliquam, porta nulla ac, consectetur ipsum. 
+                                Suspendisse et scelerisque nibh, eu sodales leo. Sed mauris neque, lobortis sed gravida eu, ultrices vel neque. 
+                                Nullam ut consectetur urna. Vestibulum dignissim interdum dictum. Vestibulum vulputate enim ante, a pulvinar velit pharetra non.
+                            </p>
+
+                            <p>
+                                Morbi congue scelerisque diam, ac tempor libero dapibus nec. Phasellus eu fermentum metus. Donec lacinia sem eget ex ultrices molestie. 
+                                Aliquam erat volutpat. Fusce laoreet vel ante non luctus. In hac habitasse platea dictumst. Aliquam erat volutpat. 
+                                Vestibulum vel orci at tellus iaculis dignissim nec ut enim. Mauris lobortis rutrum felis eget mollis. 
+                                Fusce ac dui arcu. Integer sit amet posuere nulla. Phasellus pharetra, velit eu faucibus tincidunt, neque augue scelerisque nisi, 
+                                ut rutrum nunc arcu ut mauris. Curabitur vitae leo pellentesque, dignissim urna nec, tempus metus.
+                            </p>
+
+                            <p>
+                                Vivamus accumsan ligula id elementum suscipit. Mauris in nibh eu orci pharetra tempor vel vel dolor. 
+                                Curabitur condimentum arcu vitae blandit eleifend. Nullam turpis felis, sodales bibendum leo at, porttitor efficitur ipsum. 
+                                Suspendisse eget libero porta, porttitor mauris non, molestie odio. Donec volutpat fermentum est non semper. 
+                                Cras pharetra odio ut nisi condimentum vulputate. Nunc finibus gravida magna, eu venenatis purus semper at. 
+                                Nam non enim pretium, rhoncus massa quis, tincidunt lacus.
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div><!-- end faqs -->
+        
+    </main>
+
+<?php include('includes/footer.php'); ?>
