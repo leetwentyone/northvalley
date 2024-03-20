355f6abeff1eea8b7174ed461d3de2bedf0fd0e0
leetwentyone <36470700+leetwentyone@users.noreply.github.com>
2024-03-20 14:43:36 +0000

    renamed folder

--- includes/key-messages.php
+++ includes/key-messages.php
@@ -1,19 +1,19 @@
 <div class="key-messages">
     <ul>
         <li>
-            <img src="images/tick.png">
+            <img src="images/home/key-messages/five-star-rated.png">
             <p>Five Star Rated</p>
         </li>
         <li>
-            <img src="images/tick.png">
+            <img src="images/home/key-messages/concept-to-completion.png">
             <p>Concept to Completion</p>
         </li>
         <li>
-            <img src="images/tick.png">
+            <img src="images/home/key-messages/luxury-gates.png">
             <p>Handmade Luxury Gates</p>
         </li>
         <li>
-            <img src="images/tick.png">
+            <img src="images/home/key-messages/superiro-quality.png">
             <p>Superior Quality</p>
         </li>
     </ul>
